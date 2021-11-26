136a982ecf8b759fdf8de73b5316991eeda179d1
shalomprasannan <34344441+shalomprasannan@users.noreply.github.com>
2021-11-26 21:54:37 +0530

    image

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import  { Grid, Button, Card, CardActions, CardContent, Divider, FormControl, TextField, CardMedia } from '@mui/material'
-const pexels = require('./pexels.jpeg');
+import pexels from './public/pexels.jpeg'
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
